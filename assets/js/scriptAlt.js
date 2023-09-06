fffea9fb1f3fe2b8166a6c1507dac6969597ee28
BuildTools <unconfigured@null.spigotmc.org>
2023-09-05 21:47:30 -0600

    scroll to top button

--- assets/js/scriptAlt.js
+++ assets/js/scriptAlt.js
@@ -22,7 +22,7 @@ var sec3 = document.getElementById('card3');
 var sec4 = document.getElementById('card4');
 
 function cardClick() {
-    this.remove();
+    
 }
 
 sec1.addEventListener('click', cardClick, sec1);
@@ -51,3 +51,30 @@ function myFunction(x) {
   var x = window.matchMedia("(max-width: 1000px)")
   myFunction(x) // Call listener function at run time
   x.addListener(myFunction) // Attach listener function on state changes 
+
+
+
+
+// Scroll to Top Button
+
+let mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+mybutton.style.display = "none";
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+mybutton.addEventListener('click', topFunction)

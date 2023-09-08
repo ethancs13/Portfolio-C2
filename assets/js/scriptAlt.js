38f1484a6cba401c31c2430ea83575673eb8eb15
BuildTools <unconfigured@null.spigotmc.org>
2023-09-07 22:45:55 -0600

    added functionality to card clicks

--- assets/js/scriptAlt.js
+++ assets/js/scriptAlt.js
@@ -5,7 +5,12 @@ var resume = document.getElementById("resume-link");
 var home = document.getElementById("home-link");
 var card1 = document.querySelector('#card1');
 
-var theme = document.getElementById("theme-btn");
+// Theme Button
+// var theme = document.getElementById("theme-btn");
+// function themeSwitch(){
+//   theme.href = ""
+// }
+// theme.addEventListener('click', themeSwitch);
 
 function handleClickNav() {
     this.style.fontStyle = "normal";
@@ -21,22 +26,30 @@ var sec2 = document.getElementById('card2');
 var sec3 = document.getElementById('card3');
 var sec4 = document.getElementById('card4');
 
+var cardContainer = document.querySelector("cardContainer");
+
 function cardClick() {
-    
+  var num = this.dataset.number;
+  // Store html in variable with changes based on which section we are targeting
+  const html = "<div id='H" + num + "'><h1 class='cardHeader__text-" + num + "'></h1></div><div class='cardImage' id='image" + num + "'></div>"
+
+  // when card is clicked, change html
+  if (this.innerHTML.length < 98 ){
+    this.innerHTML = "<div class=cardInfo__1 card1Alt><span>Info Card</span><div class='cardInfo__text-" + num + "'></div></div>"
+  }
+  else {
+    this.innerHTML = html; // Set html equal to html variable we made earlier
+  }
 }
+// create togglable variable
+var cardFlip = false;
 
-sec1.addEventListener('click', cardClick, sec1);
-sec2.addEventListener('click', cardClick, sec2);
-sec3.addEventListener('click', cardClick, sec3);
-sec4.addEventListener('click', cardClick, sec4);
+sec1.addEventListener('click', cardClick);
+sec2.addEventListener('click', cardClick);
+sec3.addEventListener('click', cardClick);
+sec4.addEventListener('click', cardClick);
 
-
-function themeSwitch(){
-    theme.href = ""
-}
-theme.addEventListener('click', themeSwitch);
-
-
+// Change class to mobile version
 function myFunction(x) {
     if (x.matches) { // If media query matches
         card1.classList.remove('card1Alt')
@@ -55,10 +68,9 @@ function myFunction(x) {
 
 
 // Scroll to Top Button
-
 let mybutton = document.getElementById("topBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 400px from the top of the document, show the button
 mybutton.style.display = "none";
 window.onscroll = function() {scrollFunction()};
 
